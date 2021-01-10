a36eb4adbf4dbe5791d505a681cf272ea1661a5d
tompetTom <tomfharrison@gmail.com>
2021-01-10 14:18:58 +0000

    complete mobile css homepage

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,18 @@
 $(document).ready(() => {
-    $('.instrument-category').on('click', event => {
-        if (!$(event.currentTarget).siblings().hasClass('hide')) {
-            $(event.currentTarget).siblings().slideUp().addClass('hide');
+    $('.instrument-list li').on('click', event => {
+
+        if ($(event.currentTarget).hasClass('active')) {
+            $(event.currentTarget).removeClass('active');
         } else {
-            $('.instrument-details').slideUp().addClass('hide');
-            $(event.currentTarget).siblings().slideDown().removeClass('hide');
+            $(event.currentTarget).siblings().removeClass('active');
+            $(event.currentTarget).addClass('active');
         };
+
+        // if (!$(event.currentTarget).siblings().hasClass('hide')) {
+        //     $(event.currentTarget).siblings().slideUp().addClass('hide');
+        // } else {
+        //     $('.instrument-details').slideUp().addClass('hide');
+        //     $(event.currentTarget).siblings().slideDown().removeClass('hide');
+        // };
     });
 });
