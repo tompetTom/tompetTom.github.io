b150d8154075f48a37cc501175d2d2e321eb972d
tompetTom <tomfharrison@gmail.com>
2021-01-11 08:50:34 +0000

    tweak before changing grid

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,18 +1,6 @@
 $(document).ready(() => {
     $('.instrument-list li').on('click', event => {
-
-        if ($(event.currentTarget).hasClass('active')) {
-            $(event.currentTarget).removeClass('active');
-        } else {
-            $(event.currentTarget).siblings().removeClass('active');
-            $(event.currentTarget).addClass('active');
-        };
-
-        // if (!$(event.currentTarget).siblings().hasClass('hide')) {
-        //     $(event.currentTarget).siblings().slideUp().addClass('hide');
-        // } else {
-        //     $('.instrument-details').slideUp().addClass('hide');
-        //     $(event.currentTarget).siblings().slideDown().removeClass('hide');
-        // };
+        $(event.currentTarget).siblings().removeClass('active');
+        $(event.currentTarget).toggleClass('active');
     });
 });
