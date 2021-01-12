1bb9b01f2a64b43aba53953a9a760e037e8ae71a
tompetTom <tomfharrison@gmail.com>
2021-01-12 18:17:21 +0000

    check mobile hero

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,21 @@
 $(document).ready(() => {
+    if ($(document).scrollTop() < 300) {
+        $(document).on('scroll', () => {
+            if ($(document).scrollTop() > 50) {
+                $('.landing-section').find('.tagline').addClass('fade');
+            }
+            if ($(document).scrollTop() > 200) {
+                $('#title').children().addClass('small');
+                $('header').addClass('scroll');
+                // $('.landing-section').find('.tagline').addClass('fade');
+            };
+            if ($(document).scrollTop() < 50) {
+                $('#title').children().removeClass('small');
+                $('header').removeClass('scroll');
+                $('.landing-section').find('.tagline').removeClass('fade');
+            }
+        });
+    };
     $('.instrument-list li').on('click', event => {
         $(event.currentTarget).siblings().removeClass('active');
         $(event.currentTarget).toggleClass('active');
