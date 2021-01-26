a67a481c472cdd1a942533ac1a8a9db77557d6b9
tompetTom <tomfharrison@gmail.com>
2021-01-26 20:04:34 +0000

    mostly finish first two pages

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,6 @@
 $(document).ready(() => {
+
+    // header scrolling
     if ($(document).scrollTop() < 300) {
         $(document).on('scroll', () => {
             if ($(document).scrollTop() > 50) {
@@ -17,8 +19,53 @@ $(document).ready(() => {
             };
         });
     };
+
+    // homepage instrument list
     $('.instrument-list li').on('click', event => {
         $(event.currentTarget).siblings().removeClass('active');
         $(event.currentTarget).toggleClass('active');
     });
+
+    // recording instrument list
+    $('.recording-instrument-categories li').on('click', event => {
+        let image = $(event.currentTarget);
+        let info;
+        if (image.hasClass('flutes')) {
+            info = $('#flutes-info');
+        } else if (image.hasClass('clarinets')) {
+            info = $('#clarinets-info');
+        } else if (image.hasClass('recorders')) {
+            info = $('#recorders-info');
+        } else if (image.hasClass('world-flutes')) {
+            info = $('#world-flutes-info');
+        } else if (image.hasClass('saxophones')) {
+            info = $('#saxophones-info');
+        };
+        image.siblings().removeClass('active').children('.caption').removeClass('hover');
+        image.toggleClass('active');
+        info.siblings().removeClass('active');
+        info.toggleClass('active');
+    });
+
+    $('.caption').mouseenter(event => {
+        $(event.currentTarget).addClass('hover').mouseleave(() => {
+            $(event.currentTarget).removeClass('hover');
+        });
+    });
+
+    $('.show-hide').on('click', () => {
+        $('.recording-instrument-dropdown').removeClass('active');
+        $('.recording-instrument-categories li').removeClass('active');
+    })
+
+    // recording process/gear mobile tabs
+    $('.tablist li').on('click', event => {
+        event.preventDefault();
+        if ($(event.currentTarget).hasClass('active')) {
+            return;
+        } else {
+            $('.tablist').children().toggleClass('active');
+            $('.process-gear-tabs').toggleClass('active');
+        };
+    })
 });
