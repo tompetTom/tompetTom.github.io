d3f91f1782c5b784a96133ac4be449dd74af014d
tompetTom <tomfharrison@gmail.com>
2021-02-01 18:10:56 +0000

    add recording testimonials

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -20,18 +20,39 @@ $(document).ready(() => {
         });
     };
 
+    // mobile nav
+    $('.hamburger-close').on('click', e => {
+        $(e.currentTarget).closest('nav').toggleClass('closed');
+    });
+
     // scroll to element
     function scrollToElement(link) {
-        var element = $(document).find(link.attr('href'));
-        if (link.attr('href') == '#gear') {
-            console.log('gear');
+        var target;
+        if (link.attr('href')) {
+            target = link.attr('href');
+        } else if (link.attr('id')) {
+            target = `#${link.attr('id')}`;
+        };
+        var element = $(document).find(target);
+        if (target == '#gear') {
             $('.process-gear').addClass('pg-right');
             element = $(document).find('#process');
-        } else if (link.attr('href') == '#process') {
-            console.log('process');
+        } else if (target == '#process') {
             $('.process-gear').removeClass('pg-right');
         };
-        var scrollDistance = element.offset().top - 60;
+        var windowWidth = $(window).width();
+        var windowHeight = $(window).height();
+        var headerHeight;
+        if (windowWidth < 600) {
+            headerHeight = 0;
+        } else if (windowWidth < 900 && windowHeight < windowWidth) {
+            headerHeight = 0;
+        } else if (windowWidth >= 600 && windowWidth < 900 && windowHeight > windowWidth) {
+            headerHeight = 55;
+        } else {
+            headerHeight = 67;
+        };
+        var scrollDistance = element.offset().top - headerHeight;
         $('html, body').animate({scrollTop: scrollDistance}, 500);
     };
 
@@ -41,20 +62,23 @@ $(document).ready(() => {
     });
 
     // slick
-    $('.slick-quote').slick({
+    var slickMinimal = {
         autoplay: true,
         autoplaySpeed: 5000,
         dots: false,
         arrows: false,
         speed: 1000
-    });
+    };
 
-    $('.slick-video').slick({
+    var slickControls = {
         autoplay: false,
         dots: true,
         arrows: true,
         speed: 900
-    });
+    };
+
+    $('.slick-quote').slick({slickMinimal});
+    $('.slick-video').slick({slickControls});
 
     // homepage instrument list
     $('.instrument-list li').on('click', event => {
@@ -89,14 +113,13 @@ $(document).ready(() => {
         });
     });
 
-    $('.show-hide').on('click', () => {
+    $('.collapse-instrument-dropdown').on('click', () => {
         $('#recording-instruments')[0].scrollIntoView({
-            behavior: 'smooth',
-            block: 'center'
+            behavior: 'smooth'
         });
         $('.recording-instrument-dropdown').removeClass('active');
         $('.recording-instrument-categories li').removeClass('active');
-    })
+    });
 
     // recording process/gear mobile tabs
     $('.pg-headings h2').on('click', event => {
@@ -107,11 +130,31 @@ $(document).ready(() => {
         };
     });
 
-    $('.pg-track div').on('swipeleft', () => {
+    $('.pg-track').on('swipeleft', () => {
         $('.process-gear').addClass('pg-right');
     });
 
     $('.pg-track div').on('swiperight', () => {
         $('.process-gear').removeClass('pg-right');
+    });
+
+    // show/hide button
+    $('.show-hide').on('click', e => {
+        var button = $(e.currentTarget);
+        var section = button.closest('section');
+        // var id = section.attr('id');
+        if (section.hasClass('full')) {
+            console.log('full (1)');
+            scrollToElement(section);
+            console.log('full (2)');
+            section.removeClass('full');
+            section.addClass('short');
+            console.log('full (3)');
+        } else {
+            console.log('short (1)');
+            section.removeClass('short');
+            section.addClass('full');
+            console.log('short (2)');
+        };
     });
 });
